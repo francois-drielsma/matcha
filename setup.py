b65be6a62ff553406fa08b228682ded60b058c30
Andrew Mogan <amogan@vols.utk.edu>
2023-02-14 10:22:44 -0600

    Rename to matcha

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 VER = "0.1.0"
 
 setup(
-    name="crttpcmatcher",
+    name="matcha",
     version=VER,
     author="Andrew J. Mogan",
     author_email="andrew.mogan@colostate.edu",
     description="A package for matching CRT hit information with TPC tracks.",
-    url="https://github.com/andrewmogan/crttpcmatcher",
+    url="https://github.com/andrewmogan/matcha",
     packages=find_packages(where="src"), 
     package_dir={"":"src"},
     install_requires=[],
