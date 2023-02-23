70917fd2bddf8f749139e09a8955ca48e615eedd
Andrew Mogan <andrew.mogan@colostate.edu>
2023-02-23 14:51:03 -0800

    Denote attributes as private

--- src/matcha/track.py
+++ src/matcha/track.py
@@ -1,10 +1,10 @@
 class Track:
     def __init__(self, id=-1, points_xyz_cm=None, start_xyz_cm=None, end_xyz_cm=None, average_direction=None):
-        self.id = id
-        self.points_xyz_cm = points_xyz_cm 
-        self.start_xyz_cm  = start_xyz_cm
-        self.end_xyz_cm    = end_xyz_cm
-        self.average_direction = average_direction
+        self._id = id
+        self._points_xyz_cm = points_xyz_cm 
+        self._start_xyz_cm  = start_xyz_cm
+        self._end_xyz_cm    = end_xyz_cm
+        self._average_direction = average_direction
         print('Initialized Track class')
 
     def track_pca(self, track):
