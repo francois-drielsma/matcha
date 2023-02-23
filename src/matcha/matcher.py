8d85726b3551f6a25cf0f52c33e4a181d9df316c
Andrew Mogan <andrew.mogan@colostate.edu>
2023-02-23 14:50:27 -0800

    Add Match class for storing matched Track and CRTHit

--- src/matcha/matcher.py
+++ src/matcha/matcher.py
@@ -1,4 +1,12 @@
-class Matcher:
+from track import Track
+from crthit import CRTHit
+class Match:
+    def __init__(self, track:Track(), crthit:CRTHit()):
+        self.track  = track
+        self.crthit = crthit
+        print('Matched Track ID {} with CRTHit ID {}'.format(track.id(), crthit.id()))
+
+class Matchmaker:
     def __init__(self, distance_limit_cm=100, minimum_track_length_cm=50, minimum_pe=50):
         self.distance_limit_cm = distance_limit_cm           # Distance of closest approach threshold in cm
         self.minimum_track_length_cm = minimum_track_length_cm
