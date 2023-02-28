3f3079a94b70de32a8b36fa0f3e5404d34b35ae6
Andrew Mogan <andrew.mogan@colostate.edu>
2023-02-28 10:41:19 -0800

    Use property decorator for getters/setters like an adult

--- src/matcha/matcher.py
+++ src/matcha/matcher.py
@@ -1,19 +1,58 @@
 from track import Track
 from crthit import CRTHit
+
 class Match:
-    def __init__(self, track:Track(), crthit:CRTHit()):
-        self.track  = track
-        self.crthit = crthit
+    def __init__(self, index, track, crthit):
+        self._index  = index
+        self._track  = track
+        self._crthit = crthit
         print('Matched Track ID {} with CRTHit ID {}'.format(track.id(), crthit.id()))
 
 class Matchmaker:
-    def __init__(self, distance_limit_cm=100, minimum_track_length_cm=50, minimum_pe=50):
-        self.distance_limit_cm = distance_limit_cm           # Distance of closest approach threshold in cm
-        self.minimum_track_length_cm = minimum_track_length_cm
-        self.minimum_pe = minimum_pe                   # Minimum no. photoelectrons for CRT hit
+    def __init__(self, track, crthit, distance_limit=100, minimum_track_length=50, minimum_pe=50):
+        self._track = track    
+        self._crthit = crthit  
+        self._distance_limit = distance_limit              # Distance of closest approach threshold in cm
+        self._minimum_track_length = minimum_track_length
+        self._minimum_pe = minimum_pe                            # Minimum no. photoelectrons for CRT hit
         print('Initialized Matcher class')
 
-    def calculate_dca(self, crthit): 
+    @property
+    def track(self):
+        return self._track
+    @track.setter
+    def track(self, value):
+        self._track = value
+
+    @property
+    def crthit(self):
+        return self._crthit
+    @crthit.setter
+    def crthit(self, value):
+        self._crthit = value
+
+    @property
+    def distance_limit(self):
+        return self._distance_limit
+    @distance_limit.setter
+    def distance_limit(self, value):
+        self._distance_limit = value
+
+    @property
+    def minimum_track_length(self):
+        return self._minimum_track_length
+    @minimum_track_length.setter
+    def minimum_track_length(self, value):
+        self._minimum_track_length = value
+
+    @property
+    def minimum_pe(self):
+        return self._minimum_pe
+    @minimum_pe.setter
+    def minimum_pe(self, value):
+        self._minimum_pe = value
+
+    def calculate_dca(self, track, crthit): 
         # Calculate distance of closest approach.
         # Requires shifting the track by -v*t and extrapolating to the 
         # CRT hit position.
@@ -23,6 +62,6 @@ class Matchmaker:
         # What's the input here? How do we load a track into this?
         pass
 
-    def match_crt_hits(self, track):
+    def get_crt_tpc_matches(self, tracks, crthits, distance_limit):
         # Loop over CRT hits, calculate DCA for each, determine matches
         pass
