6ceb40a56c859573ba41396e267797ae063cec04
Andrew Mogan <andrew.mogan@colostate.edu>
2023-02-28 10:33:02 -0800

    Use property decorator for getters/setters like and adult

--- src/matcha/track.py
+++ src/matcha/track.py
@@ -1,12 +1,110 @@
 class Track:
-    def __init__(self, id=-1, points_xyz_cm=None, start_xyz_cm=None, end_xyz_cm=None, average_direction=None):
+    """
+    Class for storing TPC track information. The stored tracks are
+    assumed to be muon candidates for CRT-TPC matching.
+
+    Attributes
+    ----------
+    id : int, required
+        Identifier for Track instance
+    start_x : float
+        x-position of the first track point in cm. Default: 0
+    end_x : float
+        x-position of the last track point in cm. Default: 0
+    start_y : float
+        y-position of the first track point in cm. Default: 0
+    end_y : float
+        y-position of the last track point in cm. Default: 0
+    start_z : float
+        z-position of the first track point in cm. Default: 0
+    end_z : float
+        z-position of the last track point in cm. Default: 0
+    points : array-like (N, 3)
+        List of 3D points comprising the track. Default: []
+    depositions : array-like (N, 1)
+        List of energy deposition values for each point in rescaled ADC units. Default: []
+    Methods
+    -------
+    TODO INSERT
+    """
+    # TODO What does "rescaled ADC units mean? (from Particle class)
+    def __init__(self, id, start_x=0, start_y=0, start_z=0, 
+                 end_x, end_y, end_z=0,
+                 points=[], depositions=[]):
         self._id = id
-        self._points_xyz_cm = points_xyz_cm 
-        self._start_xyz_cm  = start_xyz_cm
-        self._end_xyz_cm    = end_xyz_cm
-        self._average_direction = average_direction
+        self._start_x = start_x
+        self._start_y = start_y
+        self._start_z = start_z
+        self._end_x   = end_x
+        self._end_y   = end_y
+        self._end_z   = end_z
+        self._points = points
+        self._depositions = depositions
         print('Initialized Track class')
 
-    def track_pca(self, track):
-        pass
+    ### Getters and setters ###
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @property
+    def start_x(self):
+        return self._start_x
+    @start_x.setter
+    def start_x(self, value):
+        self._start_x = value
+
+    @property
+    def start_y(self):
+        return self._start_y
+    @start_y.setter
+    def start_y(self, value):
+        self._start_y = value
+
+    @property
+    def start_z(self):
+        return self._start_z
+    @start_z.setter
+    def start_z(self, value):
+        self._start_z = value
+
+    @property
+    def end_x(self):
+        return self._end_x
+    @end_x.setter
+    def end_x(self, value):
+        self._end_x = value
+
+    @property
+    def end_y(self):
+        return self._end_y
+    @end_y.setter
+    def end_y(self, value):
+        self._end_y = value
+
+    @property
+    def end_z(self):
+        return self._end_z
+    @end_z.setter
+    def end_z(self, value):
+        self._end_z = value
+
+    @property
+    def points(self):
+        return self._points
+    @points.setter
+    def points(self, value):
+        self._points = value
+
+    @property 
+    def depositions(self):
+        return self._depositions
+    @depositions.setter
+    def depositions(self, value):
+        self._depositions = value
+
+
 
