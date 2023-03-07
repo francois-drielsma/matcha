066724eb54c58f01f832beda40e1ab18fc037cef
Andrew Mogan <andrew.mogan@colostate.edu>
2023-03-07 15:21:21 -0800

    Update for TrackPoint refactoring and add some skeleton code

--- src/matcha/run_matchmaking.py
+++ src/matcha/run_matchmaking.py
@@ -4,26 +4,27 @@ def get_crt_tpc_matches(tracks, crthits, approach_distance_threshold=50):
 
     match_candidates = []
     for track in tracks:
+        # get_track_endpoints gives a tuple of TrackPoint instances corresponding to 
+        # the start and end points
         track_startpoint, track_endpoint = track.get_track_endpoints(track.points, track.depositions)
-        track_startdir  , track_enddir   = track.get_track_endpoint_angles(track_startpoint, track_endpoint)
-        get_track_endpoint_angles(self, track_startpoint, track_endpoint):
         for crthit in crthits:
+            dca = calculate_distance_of_closest_approach(track_startpoint, crthit)
+            if dca > approach_distance_threshold: continue
+            match_candidate = MatchCandidate(track, crthit)
+            match_candidates.append(match_candidate)
 
     return match_candidates
 
-def calculate_distance_of_closest_approach(track, crthit): 
-    track_start_x = track.start_x
-    track_start_y = track.start_y
-    track_start_z = track.start_z
-
-    track_direction_vector = track.get_track_angles()
-    # Calculate distance of closest approach.
-    # Requires shifting the track by -v*t and extrapolating to the 
-    # CRT hit position.
-    pass
+def calculate_distance_of_closest_approach(track_point, crthit): 
+    dca = 0
+    # Shift track point by -v*t 
+    track_point.shift_direction_x(crthit.t0)
+    # Find some fancy linear algebra to get the DCA
+    return dca
 
 def get_best_match(self, match_candidates):
     return min(candidate.distance_of_closest_approach for candidate in match_candidates)
 
 if __name__ == "__main__":
-    get_crt_tpc_matches(track_list, crthit_list, dca_threshold)
+    match_candidates = get_crt_tpc_matches(track_list, crthit_list, dca_threshold)
+    best_match = get_best_match(match_candidates)
