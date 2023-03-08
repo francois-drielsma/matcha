884ae57a825eb97932eef559f32252b85f4f771b
Andrew Mogan <andrew.mogan@colostate.edu>
2023-03-08 13:41:12 -0800

    Refactor matching methods
    Previously called run_matchmaking, but that's not what we actually
    want here. We just want some importable methods that can take
    match.Track and matcha.CRTHit objects and run some methods on them.

---
+++ src/matcha/match_maker.py
@@ -0,0 +1,32 @@
+import .track .crthit .match_candidate
+"""
+Collection of functions for performing CRT-TPC matching
+"""
+
+def get_crt_tpc_matches(tracks, crthits, approach_distance_threshold=50):
+    # Loop over CRT hits, calculate DCA for each, determine matches
+    # Return list of MatchCandidates
+
+    match_candidates = []
+    for track in tracks:
+        # get_track_endpoints gives a tuple of TrackPoint instances corresponding to 
+        # the start and end points
+        track_startpoint, track_endpoint = track.get_track_endpoints(track.points, track.depositions)
+        for crthit in crthits:
+            dca = calculate_distance_of_closest_approach(track_startpoint, crthit)
+            if dca > approach_distance_threshold: continue
+            match_candidate = MatchCandidate(track, crthit)
+            match_candidates.append(match_candidate)
+
+    return match_candidates
+
+def calculate_distance_of_closest_approach(track_point, crthit): 
+    dca = 0
+    # Shift track point by -v*t 
+    track_point.shift_direction_x(crthit.t0)
+    # Find some fancy linear algebra to get the DCA
+    return dca
+
+def get_best_match(self, match_candidates):
+    return min(candidate.distance_of_closest_approach for candidate in match_candidates)
+
