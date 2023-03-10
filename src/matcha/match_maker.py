105309e7c23adddd913f64cf2ba3e8aa63ae29a9
Andrew Mogan <andrew.mogan@colostate.edu>
2023-03-10 15:04:50 -0800

    Implement first pass of DCA calculation

--- src/matcha/match_maker.py
+++ src/matcha/match_maker.py
@@ -1,8 +1,11 @@
 from . import track, crthit, match_candidate
+import numpy as np
 """
 Collection of functions for performing CRT-TPC matching
 """
 
+# TODO How to handle DCA for both start and end points?
+# TODO Extrapolate in both directions?
 def get_match_candidates(tracks, crthits, approach_distance_threshold=50):
     """
     Loop over CRT hits, calculate DCA for each, determine matches
@@ -34,11 +37,18 @@ def calculate_distance_of_closest_approach(track_point, crthit):
     dca = 0
     crthit_time = crthit.GetTimeInMicroseconds()
     # Shift track point by -v*t 
-    track_point.shift_direction_x(crthit.t0)
+    track_point.shift_direction_x(crthit_time)
     # Find some fancy linear algebra to get the DCA
+    crthit_position = (crthit.position_x, crthit.position_y, crthit.position_z)
+    track_endpoint = (track_point.position_x, track_point.position_y, track_point.position_z)
+    unit_vec = (track_point.direction_x, track_point.direction_y, track_point.direction_z)
+    end = track_endpoint + unit_vec
+    #denominator = np.sqrt(unit_vec[0]**2, unit_vec[1]**2, unit_vec[2]**2)
+    denominator = np.linalg.norm(unit_vec)
+    numerator = np.cross((crthit_position - track_endpoint), np.linalg.norm(crthit_position - end))
+    dca = numerator/denominator
     return dca
 
-# TODO Add extrapolation length and use sin(dca/len) to determine best match
 def get_best_match(self, match_candidates):
     return min(candidate.distance_of_closest_approach for candidate in match_candidates)
 
