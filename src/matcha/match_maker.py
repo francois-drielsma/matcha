d9620157854b2862b1917e87861915c1742e5145
Andrew Mogan <andrew.mogan@colostate.edu>
2023-03-13 07:57:00 -0700

    Fix crt_hit name
    Importing this as "crthit" was confusing the match_maker, since crthit
    is both the module name and the variable name. Added an extra underscore
    here to differentiate.

--- src/matcha/match_maker.py
+++ src/matcha/match_maker.py
@@ -1,4 +1,8 @@
-from . import track, crthit, match_candidate
+#from . import track, crthit, match_candidate
+from .track import Track
+from .track_point import TrackPoint
+from .crthit import CRTHit
+from .match_candidate import MatchCandidate
 import numpy as np
 """
 Collection of functions for performing CRT-TPC matching
@@ -17,15 +21,16 @@ def get_match_candidates(tracks, crthits, approach_distance_threshold=50):
         # get_track_endpoints gives a tuple of TrackPoint instances corresponding to 
         # the start and end points
         track_startpoint, track_endpoint = track.get_track_endpoints()
-        for crthit in crthits:
-            dca = calculate_distance_of_closest_approach(track_startpoint, crthit)
+        for crt_hit in crthits:
+            print('type crthit:', type(crt_hit))
+            dca = calculate_distance_of_closest_approach(track_startpoint, crt_hit)
             if dca > approach_distance_threshold: continue
-            match_candidate = MatchCandidate(track, crthit)
+            match_candidate = MatchCandidate(track, crt_hit)
             match_candidates.append(match_candidate)
 
     return match_candidates
 
-def calculate_distance_of_closest_approach(track_point, crthit): 
+def calculate_distance_of_closest_approach(track_point, crt_hit, isdata=False): 
     """
     Tyler's C++ method to be ported:
         TVector3 pos (hit.x_pos, hit.y_pos, hit.z_pos);
@@ -35,17 +40,17 @@ def calculate_distance_of_closest_approach(track_point, crthit):
         return numerator/denominator;
     """
     dca = 0
-    crthit_time = crthit.GetTimeInMicroseconds()
+    crt_hit_time = crt_hit.GetTimeInMicroseconds(isdata)
     # Shift track point by -v*t 
-    track_point.shift_direction_x(crthit_time)
+    track_point.shift_position_x(crt_hit_time, isdata)
     # Find some fancy linear algebra to get the DCA
-    crthit_position = (crthit.position_x, crthit.position_y, crthit.position_z)
+    crt_hit_position = (crt_hit.position_x, crt_hit.position_y, crt_hit.position_z)
     track_endpoint = (track_point.position_x, track_point.position_y, track_point.position_z)
     unit_vec = (track_point.direction_x, track_point.direction_y, track_point.direction_z)
     end = track_endpoint + unit_vec
     #denominator = np.sqrt(unit_vec[0]**2, unit_vec[1]**2, unit_vec[2]**2)
     denominator = np.linalg.norm(unit_vec)
-    numerator = np.cross((crthit_position - track_endpoint), np.linalg.norm(crthit_position - end))
+    numerator = np.cross((crt_hit_position - track_endpoint), np.linalg.norm(crt_hit_position - end))
     dca = numerator/denominator
     return dca
 
