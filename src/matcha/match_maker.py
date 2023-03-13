Fix DCA calculation
- Numerator and denominator are both now floats instead of vectors
(accidentally)
- Loop over track start and end point instead of just start point
- Use numpy arrays like a smart person

src/matcha/match_maker.py
@@ -21,12 +21,14 @@ def get_match_candidates(tracks, crthits, approach_distance_threshold=50):
         # get_track_endpoints gives a tuple of TrackPoint instances corresponding to 
         # the start and end points
         track_startpoint, track_endpoint = track.get_track_endpoints()
-        for crt_hit in crthits:
-            print('type crthit:', type(crt_hit))
-            dca = calculate_distance_of_closest_approach(track_startpoint, crt_hit)
-            if dca > approach_distance_threshold: continue
-            match_candidate = MatchCandidate(track, crt_hit)
-            match_candidates.append(match_candidate)
+        for point in (track_startpoint, track_endpoint):
+            print('Point tpc region name:', point.tpc_region.name)
+            if point.tpc_region.name not in ['EE', 'EW', 'WE', 'WW']: continue
+            for crt_hit in crthits:
+                dca = calculate_distance_of_closest_approach(point, crt_hit)
+                if dca > approach_distance_threshold: continue
+                match_candidate = MatchCandidate(track, crt_hit)
+                match_candidates.append(match_candidate)
 
     return match_candidates
 
@@ -39,22 +41,26 @@ def calculate_distance_of_closest_approach(track_point, crt_hit, isdata=False):
         double numerator = (pos - start).Cross(pos - end).Mag();
         return numerator/denominator;
     """
+    print('[CALCDCA] track_point:', track_point)
+    print('[CALCDCA] crt_hit:', crt_hit)
+    print('[CALCDCA] track_point x:', track_point.position_x)
     dca = 0
     crt_hit_time = crt_hit.GetTimeInMicroseconds(isdata)
     # Shift track point by -v*t 
     track_point.shift_position_x(crt_hit_time, isdata)
-    # Find some fancy linear algebra to get the DCA
-    crt_hit_position = (crt_hit.position_x, crt_hit.position_y, crt_hit.position_z)
-    track_endpoint = (track_point.position_x, track_point.position_y, track_point.position_z)
-    unit_vec = (track_point.direction_x, track_point.direction_y, track_point.direction_z)
-    end = track_endpoint + unit_vec
+    print('[CALCDCA] track_point shifted x:', track_point.position_x)
+    # Do some fancy linear algebra to get the DCA
+    crt_hit_position = np.array([crt_hit.position_x, crt_hit.position_y, crt_hit.position_z])
+    track_endpoint = np.array([track_point.position_x, track_point.position_y, track_point.position_z])
+    unit_vec = np.array([track_point.direction_x, track_point.direction_y, track_point.direction_z])
+    end = np.array(track_endpoint + unit_vec)
     #denominator = np.sqrt(unit_vec[0]**2, unit_vec[1]**2, unit_vec[2]**2)
     denominator = np.linalg.norm(unit_vec)
-    numerator = np.cross((crt_hit_position - track_endpoint), np.linalg.norm(crt_hit_position - end))
+    numerator = np.linalg.norm(np.cross((crt_hit_position - track_endpoint), (crt_hit_position - end)))
     dca = numerator/denominator
     return dca
 
-def get_best_match(self, match_candidates):
+def get_best_match(match_candidates):
     return min(candidate.distance_of_closest_approach for candidate in match_candidates)
 
 