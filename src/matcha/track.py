fe3c44c3053a30a4167d14bc576414477ffd8a53
Andrew Mogan <andrew.mogan@colostate.edu>
2023-03-14 10:14:34 -0700

    Clean things up a bit
    - Remove some old comments
    - Reorder attributes in Track docstring to match the constructor

--- src/matcha/track.py
+++ src/matcha/track.py
@@ -21,14 +21,14 @@ class Track:
         Identifier for the parent interaction, or "vertex." Default: -1
     start_x : float
         x-position of the first track point in cm. Default: 0
-    end_x : float
-        x-position of the last track point in cm. Default: 0
     start_y : float
         y-position of the first track point in cm. Default: 0
-    end_y : float
-        y-position of the last track point in cm. Default: 0
     start_z : float
         z-position of the first track point in cm. Default: 0
+    end_x : float
+        x-position of the last track point in cm. Default: 0
+    end_y : float
+        y-position of the last track point in cm. Default: 0
     end_z : float
         z-position of the last track point in cm. Default: 0
     points : array-like (N, 3)
@@ -67,9 +67,6 @@ class Track:
         return (f"[Track] ID {self.id}, image_id {self.image_id}, interaction_id {self.interaction_id}\n\t"
                 f"start xyz: ({self.start_x}, {self.start_y}, {self.start_z})\n\t"
                 f"end xyz: ({self.end_x}, {self.end_y}, {self.end_z})")
-
-    def __repr__(self):
-        return f"Track({self.id}, {self.image_id}, {self.interaction_id}, {self.start_x}, {self.start_y}, {self.start_z}, {self.end_x}, {self.start_y}, {self.start_z}, {self.points}, {self.depositions})"
 
     ### Getters and setters ###
     @property
@@ -188,12 +185,9 @@ class Track:
 
         def get_track_point_angles(start_point, end_point, points, radius, min_points_in_radius):
             pca = PCA(n_components=2)
-            #endpoints = get_endpoints(points, depositions, radius)
             directions = []
-            #for p in endpoints:
             for point in (start_point, end_point):
                 mask = cdist([point], points)[0] < radius
-                #if np.sum(mask) > 2:
                 if np.sum(mask) < min_points_in_radius:
                     directions.append(np.array([-9999.0, -9999.0, -9999.0]))
                     continue
@@ -234,12 +228,16 @@ class Track:
         if np.argmin(local_density) == 1:
             #local_density.reverse()
             candidates = np.flip(candidates, axis=0)
-        print('Final candidates:', candidates)
+        print('[GETENDPOINTS] Final candidates:', candidates)
 
         start_point, end_point = candidates[0], candidates[1]
         angles = get_track_point_angles(start_point, end_point, points, radius, min_points_in_radius)
 
         start_direction, end_direction = angles[0], angles[1]
+
+        # Update parent track attributes
+        self.start_x, self.start_y, self.start_z = start_point[0], start_point[1], start_point[2]
+        self.end_x, self.end_y, self.end_z = end_point[0], end_point[1], end_point[2]
 
         # Construct TrackPoint instances to store the position and direction
         track_start_point = TrackPoint(
@@ -264,109 +262,6 @@ class Track:
 
         return track_start_point, track_end_point
 
-    #def get_track_endpoints(self, points, depositions, radius=20):
-    def get_track_endpoints_old(self, radius=20):
-        """
-        Calculates the start/end points of the track using local charge
-        density to guess at the Bragg peak.
-
-        Parameters
-        ----------
-        points : array_like, shape (n_points, 3)
-            The 3D space points of the track.
-        depositions : array_like, shape (n_points,)
-            The charge depositions corresponding to the points.
-        radius : int, optional
-            Radius (in voxels) for local charge density calculation.
-
-        Return
-        ------
-        A list with two numpy arrays of shape (3,) containing the start
-        and end point (respectively).
-        """
-        if not self.points.any():
-            raise ValueError('Track points attribute must be filled before calling get_track_endpoints')
-        if not self.depositions.any():
-            raise ValueError('Track depositions attribute must be filled before calling get_track_endpoints')
-
-        points = self.points
-        depositions = self.depositions 
-
-        print('points shape BEFORE local density:', points.shape)
-        print('depositions shape:', depositions.shape)
-
-        def get_local_density(candidates, points, depositions, radius):
-            local_density = []
-            print('point shape IN local density:', points.shape)
-            for candidate in candidates:
-                print('initial candidate', candidate)
-                mask = cdist([candidate], points) < radius
-                print('Mask shape before reshape:', mask.shape)
-                # The above line gives mask an extra axis which must be 
-                # removed in order to index 'points' properly
-                #mask = mask.reshape(-1)
-                mask = mask.flatten()
-                print('Mask shape after reshape:', mask.shape)
-                print('mask shape IN local density:', mask.shape)
-                if np.sum(mask) > 10:
-                    local_projection = pca.fit_transform(points[mask])
-                    local_candidates = points[mask][np.argmin(local_projection[:, 0])], \
-                                       points[mask][np.argmax(local_projection[:, 0])]
-                    print('local candidates:', local_candidates)
-                    candidate = local_candidates[np.argmin(cdist([candidate], local_candidates))]
-                    print('Revised candidate', candidate)
-                    mask = cdist([candidate], points) < radius
-                local_density.append(np.sum(depositions[mask]))
-            return local_density
-
-        pca = PCA(n_components=2)
-        projection = pca.fit_transform(points)
-        print('projection type:', type(projection))
-        print('projection dtype:', projection.dtype)
-        candidates = [points[np.argmin(projection[:, 0])], points[np.argmax(projection[:, 0])]]
-        #candidates = np.array(points[np.argmin(projection[:, 0])], points[np.argmax(projection[:, 0])])
-        print('candidates type:', type(candidates))
-        print('candidates:\n', candidates)
-        candidates = np.array(candidates)
-        local_density = get_local_density(candidates, points, depositions, radius)
-        if np.argmin(local_density) == 1:
-            local_density.reverse()
-            candidates.reverse()
-
-        return candidates
-
-    def get_track_angles(points, depositions, radius):
-        """
-        Calculates the approximate angle of the track using a local
-        PCA about each endpoint.
-
-        Parameters
-        ----------
-        points: The 3D space points of the track
-        depositions: The charge depositions corresponding to the points.
-        radius: Radius used for local primary direction calculation.
-
-        Return
-        ------
-        The primary components of the track (starting at both ends),
-        a bool flagging the first endpoint as having the lowest local
-        charge density, and the calculated endpoints.
-        """
-        pca = PCA(n_components=2)
-        endpoints = get_endpoints(points, depositions, radius)
-        ret = list()
-        centroid = list()
-        p0_localQ_lowest = True
-        for p in endpoints:
-            mask = cdist([p], points)[0] < radius
-            if np.sum(mask) > 2:
-                centroid.append(np.mean(points[mask], axis=0))
-                primary = pca.fit(points[mask]).components_[0]
-                ret.append(primary / np.linalg.norm(primary))
-            else:
-                ret.append(np.array([-9999.0, -9999.0, -9999.0]))
-                centroid.append(np.array([0,0,0]))
-        return ret, p0_localQ_lowest, endpoints
 
 
 
