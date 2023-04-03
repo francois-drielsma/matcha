b618bf22ce5c99f8d60315dcbd62ac5186dbc314
Andrew Mogan <andrew.mogan@colostate.edu>
2023-04-03 08:27:31 -0700

    Finish CRTHit writer and add docstrings

--- src/matcha/writer.py
+++ src/matcha/writer.py
@@ -1,3 +1,4 @@
+import os
 from .track import Track
 from .track_point import TrackPoint
 from .crthit import CRTHit
@@ -5,7 +6,21 @@ from .match_candidate import MatchCandidate
 from .writer import write_to_file
 import numpy as np
 
-def write_tracks_to_file(tracks):
+def write_tracks_to_file(tracks, file_path):
+    """
+    Write a list of Track instances to a NumPy file 
+
+    Args
+    ----
+    tracks : list 
+        A list of Track instances.
+    file_path : str 
+        Output path to store the Numpy file. Defaults to current directory if
+        file_path does not exist
+
+    Returns:
+        None.
+    """
     data = {
         'track_id': [],
         'track_image_id': [],
@@ -34,9 +49,23 @@ def write_tracks_to_file(tracks):
         data['track_points'].append(track.points)
         data['track_depositions'].append(track.depositions)
     
-    np.save('tracks.npy', data)
+    np.save(file_path+'/tracks.npy', data)
 
-def write_crthits_to_file(tracks):
+def write_crthits_to_file(crthits, file_path):
+    """
+    Write a list of CRTHit instances to a NumPy file
+
+    Args
+    ----
+    crthits : list 
+        A list of CRTHit instances.
+    file_path : str 
+        Output path to store the NumPy file. Defaults to current directory if
+        file_path does not exist
+
+    Returns:
+        None.
+    """
     data = {
         'crthit_id': [],
         'crthit_total_pe': [],
@@ -50,22 +79,33 @@ def write_crthits_to_file(tracks):
         'crthit_error_y': [],
         'crthit_error_z': [],
         'crthit_plane': [],
-        'crthit_tagger': []
+        'crthit_tagger': [] 
     }
-
-    # TODO Make ChatGPT do this lul
-    # Convert track objects to dictionary of lists
-    for track in tracks:
-        data['track_id'].append(track.id)
-        data['track_image_id'].append(track.image_id)
-        data['track_interaction_id'].append(track.interaction_id)
-        data['track_start_x'].append(track.start_x)
-        data['track_start_y'].append(track.start_y)
-        data['track_start_z'].append(track.start_z)
-        data['track_end_x'].append(track.end_x)
-        data['track_end_y'].append(track.end_y)
-        data['track_end_z'].append(track.end_z)
-        data['track_points'].append(track.points)
-        data['track_depositions'].append(track.depositions)
     
-    np.save('tracks.npy', data)
+    for crthit in crthits:
+        data['crthit_id'].append(crthit.id)
+        data['crthit_total_pe'].append(crthit.total_pe)
+        data['crthit_t0_sec'].append(crthit.t0_sec)
+        data['crthit_t0_ns'].append(crthit.t0_ns)
+        data['crthit_t1_ns'].append(crthit.t1_ns)
+        data['crthit_position_x'].append(crthit.position_x)
+        data['crthit_position_y'].append(crthit.position_y)
+        data['crthit_position_z'].append(crthit.position_z)
+        data['crthit_error_x'].append(crthit.error_x)
+        data['crthit_error_y'].append(crthit.error_y)
+        data['crthit_error_z'].append(crthit.error_z)
+        data['crthit_plane'].append(crthit.plane)
+        data['crthit_tagger'].append(crthit.tagger)
+    
+    np.save(file_path+'/crthits.npy', data)
+
+
+def write(tracks, crthits, file_path):
+    if not os.path.exists(file_path):
+        print('WARNING Output file path', file_path, 'does not exist. Defaulting to current directory')
+        file_path = './'
+    write_tracks_to_file(tracks, file_path)
+    write_crthits_to_file(crthits, file_path)
+
+
+
