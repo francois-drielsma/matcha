9a45447b522f5e3e11d9a5884ea7f5ca60a2d135
Andrew Mogan <andrew.mogan@colostate.edu>
2023-04-14 09:55:58 -0700

    Move main DCA calculation to dca_methods

--- src/matcha/dca_methods.py
+++ src/matcha/dca_methods.py
@@ -3,6 +3,36 @@ from .track_point import TrackPoint
 from .crthit import CRTHit
 import numpy as np
 
+def calculate_distance_of_closest_approach(track_point, crt_hit, dca_method, 
+                                           trigger_timestamp, isdata): 
+    """
+    Calculate distance of closest approach between a CRTHit and a line segment
+    defined by the track end point and direction.
+    
+    See https://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
+    for the equation and derivation.
+
+    Parameters
+    ----------
+    track_point : matcha.TrackPoint
+        Track end point from Track.get_endpoints() 
+    crt_hit : matcha.CRTHit
+        CRT hit to which we calculate distance of closest approach
+    isdata : bool
+        Whether to run on simulation or data. Deteremines which value of
+        drift velocity to use when getting CRT hit time.
+
+    Return
+    ------
+    float value of distance of closest approach.
+    """
+
+    if dca_method == 'simple':
+        dca = simple_dca(track_point, crt_hit, trigger_timestamp, isdata)
+        return dca
+    else:
+        raise ValueError('dca_method parameter must be one of')
+
 def simple_dca(track_point, crt_hit, trigger_timestamp, isdata):
 
     crt_hit_time = crt_hit.get_time_in_microseconds(trigger_timestamp, isdata)
