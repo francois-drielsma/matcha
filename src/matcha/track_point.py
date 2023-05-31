bea379b0fcfc3ac44c8a6c6e81c8e92a82d78d76
Andrew Mogan <andrew.mogan@colostate.edu>
2023-05-31 15:40:43 -0700

    Update docstrings

--- src/matcha/track_point.py
+++ src/matcha/track_point.py
@@ -9,6 +9,15 @@ from enum import Enum
 class TPCRegion(Enum):
     """
     Class for determining which TPC or region a track endpoint lies in.
+
+    Possible values:
+        WestOfWW: Endpoint is west of the west-west TPC (outside TPCs).
+        WW: Endpoint is within the west-west TPC.
+        WE: Endpoint is within the west-east TPC.
+        BetweenTPCs: Endpoint is between the TPCs (outside TPCs).
+        EW: Endpoint is within the east-west TPC.
+        EE: Endpoint is within the east-east TPC.
+        EastOfEE: Endpoint is east of the EE cryostat (outside TPCs).
     """
     WestOfWW    = 0
     WW          = 1
@@ -20,43 +29,28 @@ class TPCRegion(Enum):
 
 class TrackPoint:
     """
-    Class for storing and managing track end point information.
+    Class for storing and managing track point information, particularly
+    start and end points and their directions.
 
-    The track end points are used in CRT-TPC matching. For each point,
-    we calculate the distance of closest approach between it and each
-    CRT hit in the image (i.e., event). This requires calculating the
-    endpoint directions based on PCA and local charge information, as
-    well as determinig the point drift direction based on which TPC
-    region it lies in. 
+    Attributes:
+        track_id (int): ID of the corresponding track this point came from.
+        position_x (float): x-position of the point in cm.
+        position_y (float): y-position of the point in cm.
+        position_z (float): z-position of the point in cm.
+        direction_x (float): x-direction of the point in cm.
+        direction_y (float): y-direction of the point in cm.
+        direction_z (float): z-direction of the point in cm.
+        tpc_region (Enum): Enum class that gives the TPC drift region,
+            e.g., EE, EW, etc. Initialized in the constructor based
+            on the TrackPoint position_x.
+        drift_direction (int): +1 or -1, depending on which TPC region the
+            point lies in. 
 
-    Attributes
-    ----------
-    track_id : int
-        ID of the corresponding track this point came from
-    position_x : float
-        x-position of the point in cm. 
-    position_y : float
-        y-position of the point in cm. 
-    position_z : float
-        z-position of the point in cm. 
-    direction_x : float
-        x-direction of the point in cm. 
-    direction_y : float
-        y-direction of the point in cm. 
-    direction_z : float
-        z-direction of the point in cm. 
-    tpc_region : Enum
-        Enum class that gives the TPC drift region, i.e., EE, EW, WE, or WW.
-        Initialized in the constructor based on the TrackPoint positition_x.
-    drift_direction: int
-        +1 or -1, depending on which TPC region the point lies in. See
-        the endpoint_drift_direction() method for details.
-
-    Methods
-    -------
-    shift_position_x(t0, isdata):
-        Shift the point position_x based on t0 and drift velocity. 
+    Methods:
+        shift_position_x(t0, isdata):
+            Shifts the point position_x based on t0 and drift velocity.
     """
+
     def __init__(self, track_id,
                  position_x, position_y, position_z,
                  direction_x, direction_y, direction_z):
@@ -143,18 +137,11 @@ class TrackPoint:
 
     def is_valid(self):
         """
-        Check if TrackPoint position and direction attributes are filled. Used 
-        to determine whether or not to estimate endpoint positions and directions
-        using PCA.
+        Check if TrackPoint position and direction attributes are filled. Used to determine
+        whether or not to estimate endpoint positions and directions using PCA.
 
-        Parameters
-        ----------
-        None
-
-        Return
-        ------
-        Bool
-            True if all positions and directions are filled, else False.
+        Returns:
+            bool: True if all positions and directions are filled, False otherwise.
         """
         if self.position_x is None or self.position_y is None or self.position_z is None \
         or self.direction_x is None or self.direction_y is None or self.direction_z is None:
@@ -163,28 +150,33 @@ class TrackPoint:
         return True
 
     def _get_tpc_region(self, point_x):
+        """
+        Method to determine the TPC region in which a given point lies based on its x-coordinate.
+        The x-coordinate boundaries for each region are defined by the TPC_X_BOUNDS list.
+
+        Parameters:
+            point_x (float): x-coordinate of the point in cm.
+
+        Returns:
+            TPCRegion: Enum class representing the TPC region in which the point lies.
+        """
         point_region = np.digitize(point_x, TPC_X_BOUNDS)
         region = TPCRegion(point_region)
         return region
 
     def _get_drift_direction(self, tpc_region):
         """
-        Method to determine which direction the ionization electrons from
-        a point will drift based on which TPC it resides in. In ICARUS, 
-        the origin is between the two TPCs. The x-values run negative-to-positive 
-        east-to-west, so a point in a west-drifting TPC will have a positive 
-        value, and vice versa. The region x-boundaries in cm are assumed to be
-        [-358.49, -210.215, -61.94, 61.94, 210.215, 358.49].
+        Method to determine which direction the ionization electrons from a point
+        will drift based on which TPC it resides in. In ICARUS, the origin is between
+        the two TPCs. The x-values run negative-to-positive east-to-west, so a point
+        in a west-drifting TPC will have a positive value, and vice versa. The region
+        x-boundaries in cm are assumed to be [-358.49, -210.215, -61.94, 61.94, 210.215, 358.49].
 
-        Parameters
-        ----------
-        tpc_region: Enum
-            TPC region in which the point lies. Determined from _get_tpc_region
+        Parameters:
+            tpc_region (Enum): TPC region in which the point lies. Determined from _get_tpc_region.
 
-        Return
-        ------
-        int or None
-            +1 or -1 if the point is inside an active TPC volume, else None.
+        Returns:
+            int or None: +1 or -1 if the point is inside an active TPC volume, else None.
         """
 
         region_name = tpc_region.name
@@ -198,30 +190,32 @@ class TrackPoint:
         else:
             return None
 
-    def shift_position_x(self, t0, isdata):
+    def shift_position_x(self, t0, isdata=False):
         """
-        Method to shift the track endpoint x-position  along the drift
-        direction according to t0 and drift velocity.
+        Method to shift the x-position of the track endpoint along the drift direction
+        based on t0 and drift velocity.
 
-        Parameters
-        ----------
-        t0 : float
-            t0 of the track point. If the corresponding track does not cross
-            the cathode, the t0 is assumed to be the CRT hit time for the
-            purpose of shifting the track and determining whether it matches
-            the CRT hit. 
-        isdata: bool, optional
-            False is running on simulation, true if running on data. Changes
-            the drift velocity value if True. 
+        Parameters:
+            t0 (float): t0 of the track point. If the corresponding track does not cross
+                        the cathode, the t0 is assumed to be the CRT hit time for the
+                        purpose of shifting the track and determining whether it matches
+                        the CRT hit.
+            isdata (bool, optional): Flag indicating whether the code is running on data
+                                     (True) or simulation (False). It affects the drift 
+                                     velocity value used for the shift. Default: False
+
+        Returns:
+            float: Shifted x-position of the track endpoint.
         """
         global DRIFT_VELOCITY
-        if isdata: DRIFT_VELOCITY = 0.157565
+        if isdata:
+            DRIFT_VELOCITY = 0.157565
 
-        position_x      = self.position_x
+        position_x = self.position_x
         drift_direction = self.drift_direction
 
         shifted_x = position_x + DRIFT_VELOCITY * t0 * drift_direction
-        
+
         return shifted_x
 
 
